00c58c60fd298280a0aa1da2ac640152227c4fb8
Kevin Boissonneault <1865974@cegeplimoilou.ca>
2024-11-14 14:29:37 -0500

    Tutorial finish

---
+++ SimpleBookCatalog/SimpleBookCatalog.Infrastructure/Context/SimpleBookCatalogDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBookCatalog.Domain.Entities;
+
+namespace SimpleBookCatalog.Infrastructure.Context
+{
+    public class SimpleBookCatalogDbContext : DbContext
+    {
+        public SimpleBookCatalogDbContext(DbContextOptions<SimpleBookCatalogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Book>().Property(b => b.Author).IsRequired().HasMaxLength(100);
+        }
+    }  
+    
+}
